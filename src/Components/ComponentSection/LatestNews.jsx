b666424279cd2b6321f717bf59cd115edc5fc8dd
theprosanto <theprosanto@gmail.ccom>
2026-05-05 14:18:19 +0600

    updated

--- src/Components/ComponentSection/LatestNews.jsx
+++ src/Components/ComponentSection/LatestNews.jsx
@@ -1,39 +1,42 @@
 import React from 'react'
 import Marquee from "react-fast-marquee";
+import Container from '../Container';
 
 
 const LatestNews = () => {
   return (
-    <div className='bg-purple-600'>
+      <div className='bg-purple-600'>
+    <Container>
         
         <ul className='flex gap-7.5 text-white h-20.25 items-center px-10'>
           
           <li className='flex gap-7.5 items-center'>
             <img src="src/assets/icon-asterisk.png" alt=""/>
-            <h1 className='text-[30px] font-bold'>Latest Updates</h1>
+            <h1 className='text-[26px] font-bold'>Latest Updates</h1>
           </li>
 
           <li className='flex gap-7.5 items-center'>
             <img src="src/assets/icon-asterisk.png" alt=""/>
-            <h1 className='text-[30px] font-bold'>New Announcements</h1>
+            <h1 className='text-[26px] font-bold'>New Announcements</h1>
           </li>
 
           <li className='flex gap-7.5 items-center'>
             <img src="src/assets/icon-asterisk.png" alt=""/>
-            <h1 className='text-[30px] font-bold'>Workshop Alerts</h1>
+            <h1 className='text-[26px] font-bold'>Workshop Alerts</h1>
           </li>
 
           <li className='flex gap-7.5 items-center'>
             <img src="src/assets/icon-asterisk.png" alt=""/>
-            <h1 className='text-[30px] font-bold'>Live Notices</h1>
+            <h1 className='text-[26px] font-bold'>Live Notices</h1>
           </li>
 
           <li className='flex gap-7.5 items-center'>
             <img src="src/assets/icon-asterisk.png" alt=""/>
-            <h1 className='text-[30px] font-bold'>Event Countdown</h1>
+            <h1 className='text-[26px] font-bold'>Event Countdown</h1>
           </li>
 
         </ul>
+    </Container>
     </div>
   )
 }
